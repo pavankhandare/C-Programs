e500069c4099fdb055bd259d326afb5b0fa014db
Pavan Khandare <55012303+pavankhandare@users.noreply.github.com>
2020-07-24 08:47:38 +0530

    There are two different logic to find palindrome number

--- palindrome.c
+++ palindrome.c
@@ -26,3 +26,33 @@ int main()
 	}
 	return(0);
 }
+// Another one logic to find number is palindrome or not.
+
+#include<stdio.h>
+
+int main()
+{
+	int num, rem = 0, n, sum = 0; // At initial state we need the number to check, 
+									//n to store that number, 
+									//rem to store the reminder and 
+									//sum to store the revers number.
+	printf("Please enter the number....\n"); // We are taking input from user.
+	scanf("%d",&num); //Checking the inserted number.
+	n = num; // Storeing the number in the n variable.
+	
+	while(n > 0) // Checkingcondition that if number is greater than 0, if yes loop will be executed.
+	{
+		rem = n % 10; // Taking reminder to get last digit of the number.
+		sum = sum * 10 + rem;
+		n = n / 10;
+	}
+	if(num == sum) // Chacking the entered number and the revers number is equal or not.
+	{
+		printf("The number is a palindrome"); // If number are same.
+	}
+	else
+	{
+		printf("The given number is not palindrome"); // If number is not same.
+	}
+	return(0);
+}
